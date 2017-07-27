f7a272e12c54dfb710caa116e2df2d374b9976be
Andrew <andrew.r224@gmail.com>
2017-07-27 08:15:51 +0300

    add #my_count method

--- enumerable.rb
+++ enumerable.rb
@@ -35,7 +35,7 @@ module Enumerable
     self
   end
 
-  def my_select(&block)
+  def my_select
     type = self.class
     # create an empty array that will later be populated...
     result = []
@@ -44,7 +44,7 @@ module Enumerable
       return "#<Enumerator: #{self}:my_select>" unless block_given?
 
       self.my_each do |item|
-        if block.call(item)
+        if yield(item)
           result << item
         end
       end
@@ -54,7 +54,7 @@ module Enumerable
       # or changed into a Hash and populated
       result = {}
       self.my_each do |key, value|
-        if block.call(key, value)
+        if yield(key, value)
           result[key] = value
         end
       end
@@ -63,7 +63,7 @@ module Enumerable
     result
   end
 
-  def my_all?(&block)
+  def my_all?
     type = self.class
     # returns true unless something is falthy
     result = true
@@ -72,11 +72,11 @@ module Enumerable
     if block_given?
       if type == Array || type == Range
         self.my_each do |item|
-          result = false unless block.call(item)
+          result = false unless yield item
         end
       elsif type == Hash
         self.my_each do |key, value|
-          result = false unless  block.call(key, value)
+          result = false unless  yield(key, value)
         end
       end
     end
@@ -84,7 +84,7 @@ module Enumerable
     result
   end
 
-  def my_any?(&block)
+  def my_any?
     type = self.class
     # returns false unless something is thruthy
     result = false
@@ -92,11 +92,11 @@ module Enumerable
     if block_given?
       if type == Array || type == Range
         self.my_each do |item|
-          result = true if block.call(item)
+          result = true if yield item
         end
       elsif type == Hash
         self.my_each do |key, value|
-          result = true if  block.call(key, value)
+          result = true if  yield(key, value)
         end
       end
     else
@@ -107,7 +107,7 @@ module Enumerable
     result
   end
 
-  def my_none?(&block)
+  def my_none?
     type = self.class
     # returns false unless something is thruthy
     result = true
@@ -116,17 +116,82 @@ module Enumerable
     if block_given?
       if type == Array || type == Range
         self.my_each do |item|
-          result = false if block.call(item)
+          result = false if yield item
         end
       elsif type == Hash
         self.my_each do |key, value|
-          result = false if  block.call(key, value)
+          result = false if  yield(key, value)
         end
       end
-    else
+    elseresult = []
+
+    if type == Array || type == Range
+      return "#<Enumerator: #{self}:my_select>" unless block_given?
+
+      self.my_each do |item|
+        if yield(item)
+          result << item
+        end
+      end
+    elsif type == Hash
+      return "#<Enumerator: #{self}:my_select>" unless block_given?
+
+      # or changed into a Hash and populated
+      result = {}
+      self.my_each do |key, value|
+        if yield(key, value)
+          result[key] = value
+        end
+      end
+    end
+
+    result
       result = false
     end
 
+    result
+  end
+
+  def my_count(arg = nil)
+    type = self.class
+
+    if block_given?
+      # create a temporary array that will later be populated and measured
+      temp = []
+
+      if type == Array || type == Range
+        self.my_each do |item|
+          if yield(item)
+            temp << item
+          end
+        end
+      elsif type == Hash
+        # or changed into a Hash and populated
+        temp = {}
+        self.my_each do |key, value|
+          if yield(key, value)
+            temp[key] = value
+          end
+        end
+      end
+
+      result = temp.length
+    elsif arg != nil
+      if type == Array || type == Range
+        temp = self.my_select do |item|
+          item == arg
+        end
+        result = temp.length
+      elsif type == Hash
+        result = 0
+      end
+    else
+      if type == Array || type == Hash
+        result = self.length
+      elsif type == Range
+        result = self.to_a.length
+      end
+    end
     result
   end
 
@@ -137,14 +202,14 @@ x = [1,2,3,5,6]
 y = {key1: 1, key2: 2, key3: 3}
 z = (1..9)
 
-x.my_none? do |e|
-  e > 5
+x.my_count do |e|
+  e >= 2
 end
 
-z.my_none? do |e|
-  e >= 9
+z.my_count do |e|
+  e <= 9
 end
 
-y.my_none? do |k, v|
-  v > 12
+y.my_count do |k, v|
+  v > -2
 end
